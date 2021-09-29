f58e318cb811cea2d5357ead2258eae85b175688
Helbert Fernandes <helbertfernandes@gmail.com>
2021-09-29 15:24:30 -0300

    Ajustes

--- shlib/compactScripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -e "$1/.compact" ]; then
-	rm -f $1/.compact
-	mkdir -p /mnt/storage/backup/$1
-	tar cjf /mnt/storage/backup/$1/development_`date "+%Y%m%d"`.tar.bz2 $1
-fi
--- shlib/exeTextAdd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. `dirname $0`/common.sh
-
-[ ! "$1" ] && exit
-echo "$@" >> "$FILE_EXE"
